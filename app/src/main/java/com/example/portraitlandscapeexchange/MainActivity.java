1afd34d62558fcb08dff6f06ab00bf210fe2ca1b
XiongShao <jerry29221979@gmail.com>
2021-05-06 15:08:40 +0800

    Initial commit

---
+++ app/src/main/java/com/example/portraitlandscapeexchange/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.portraitlandscapeexchange;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    Button left, right;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        left = findViewById(R.id.leftChange);
+        right = findViewById(R.id.rightChange);
+
+        left.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+            }
+        });
+
+        right.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+            }
+        });
+    }
+}
